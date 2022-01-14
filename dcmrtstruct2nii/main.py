401bdd4ae08c771924896d7eba01824f37e55049
Mathis Rasmussen <mathis@oncology.au.dk>
2022-01-14 11:48:15 +0100

    thread error fixed

--- dcmrtstruct2nii/main.py
+++ dcmrtstruct2nii/main.py
@@ -8,6 +8,7 @@ from multiprocessing import Pool
 
 dcm_folder = sys.argv[1]
 nii_folder = sys.argv[2]
+threads = int(os.environ.get("THREADS"))
 
 print(f"RTSTRUCT Dicom folder: {dcm_folder}")
 print(f"Nifti folder: {nii_folder}")
@@ -38,7 +39,7 @@ def check_if_rtstruct(f):
 def find_all_rtstructs(dcm):
     ## Get all subs with dicom files inside
     ## Find the shit of rtstructs
-    p = Pool(os.environ["THREADS"])
+    p = Pool(threads)
     rtstructs = p.map(check_if_rtstruct, [os.path.join(fol, f) for fol, subs, files in os.walk(dcm) for f in files])
     p.close()
     p.join()
@@ -50,22 +51,22 @@ def zip_in_and_out(rtstruct_paths, out_path):
     zipped = []
     for i, r in enumerate(rtstruct_paths):
         with pydicom.filereader.dcmread(r, force=True) as ds:
-            pid =  ds.PatientID
+            pid = ds.PatientID
         
         out = os.path.join(out_path, f"{i}_{pid}")
         zipped.append((r, out))
-    return zipped
+    yield zipped
 
 file_paths = find_all_rtstructs(dcm_folder)
 file_paths = set(file_paths)
 if None in file_paths:
     file_paths.remove(None)
 
-zipped = zip_in_and_out(file_paths, nii_folder)
+#zipped = zip_in_and_out(file_paths, nii_folder)
 
 ## Convert the shit out of rtstructs
-p = Pool(os.environ["THREADS"])
-conversion = p.starmap(extract_to_nii, zipped)
+p = Pool(threads)
+conversion = p.starmap(extract_to_nii, zip_in_and_out(file_paths, nii_folder))
 p.close()
 p.join()
 
